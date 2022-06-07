92979f847cf6f18f6815333f5ba05f122e2cde0d
Danila Kalugin <danilacalugin@outlook.com>
2022-06-07 09:12:08 +0300

    Added view ExpandedAtBats

---
+++ VKR.EF.DAO/Migrations/20220606094948_Added-View-ExpandedAtBats.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VKR.EF.DAO.Migrations
+{
+    public partial class AddedViewExpandedAtBats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"CREATE VIEW ExpandedAtBats
+AS
+SELECT        dbo.AtBats.AtBatID, dbo.AtBats.Match, B.TeamID AS Offense, B.PlayerID AS Batter, dbo.AtBats.AtBatResult, P.TeamID AS Defense, P.PlayerID AS Pitcher, dbo.AtBats.Outs, dbo.AtBats.RBI, dbo.AtBats.Inning
+FROM            dbo.AtBats INNER JOIN
+                         dbo.PlayersInTeams AS B ON B.PlayerInTeamID = dbo.AtBats.Batter INNER JOIN
+                         dbo.PlayersInTeams AS P ON P.PlayerInTeamID = dbo.AtBats.Pitcher"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("drop view ExpandedAtBats");
+        }
+    }
+}
---
+++ VKR.EF.Entities/Views/ExpandedAtBat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace VKR.EF.Entities
+{
+    public partial class ExpandedAtBat
+    {
+        public int AtBatId { get; set; }
+        public int Match { get; set; }
+        public string Offense { get; set; }
+        public int Batter { get; set; }
+        public int AtBatResult { get; set; }
+        public string Defense { get; set; }
+        public int Pitcher { get; set; }
+        public int Outs { get; set; }
+        public int Rbi { get; set; }
+        public int Inning { get; set; }
+    }
+}
