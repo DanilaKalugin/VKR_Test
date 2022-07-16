6109af50b78bffc0d3bd1300085a5de30dca7a17
Danila Kalugin <danilacalugin@outlook.com>
2022-07-16 13:43:25 +0300

    Implemented methods GetAllStadium and GetStadiumForThisMatch

--- VKR.BLL.NET5/StadiumsBL.cs
+++ VKR.BLL.NET5/StadiumsBL.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using VKR.DAL.NET5;
-using VKR.Entities.NET5;
+using VKR.EF.DAO;
+using VKR.EF.Entities;
 
 namespace VKR.BLL.NET5
 {
     public class StadiumsBL
     {
-        private readonly StadiumsDAO _stadiumsDAO = new();
+        private readonly StadiumsEFDAO _stadiumsDAO = new();
 
         public List<Stadium> GetAllStadiums() => _stadiumsDAO.GetAllStadiums().OrderBy(stadium => stadium.StadiumTitle).ToList();
+
+        public Stadium GetHomeStadiumForThisTeamAndTypeOfMatch(Team team, TypeOfMatchEnum typeOfMatch) => _stadiumsDAO.GetHomeStadiumForThisTeamAndTypeOfMatch(team.TeamAbbreviation, typeOfMatch);
     }
 }
