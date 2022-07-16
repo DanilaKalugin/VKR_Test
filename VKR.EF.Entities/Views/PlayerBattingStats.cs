a398bce50886937247382a12d292b2d725a65e34
Danila Kalugin <danilacalugin@outlook.com>
2022-07-16 13:57:45 +0300

    Removed unnecessary usings in ManInTeam and PlayerBattingStats

--- VKR.EF.Entities/Views/PlayerBattingStats.cs
+++ VKR.EF.Entities/Views/PlayerBattingStats.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Data.SqlClient.DataClassification;
-
-namespace VKR.EF.Entities
+﻿namespace VKR.EF.Entities
 {
     public class PlayerBattingStats: BattingStats
     {
