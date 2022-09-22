7ae3cee5226fbf5867a23b766e642b02f4664a73
Danila Kalugin <danilacalugin34@gmail.com>
2022-09-22 11:10:35 +0300

    Added entity StadiumFactor

---
+++ VKR.EF.DAO/Migrations/20220907084208_Add-Entity-StadiumFactor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VKR.EF.DAO.Migrations
+{
+    public partial class AddEntityStadiumFactor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StadiumFactors",
+                columns: table => new
+                {
+                    Stadium = table.Column<short>(type: "smallint", nullable: false),
+                    HittingFactor = table.Column<float>(type: "real", nullable: false, defaultValue: 1f),
+                    SingleFactor = table.Column<float>(type: "real", nullable: false, defaultValue: 1f),
+                    DoubleFactor = table.Column<float>(type: "real", nullable: false, defaultValue: 1f),
+                    TripleFactor = table.Column<float>(type: "real", nullable: false, defaultValue: 1f),
+                    HomeRunFactor = table.Column<float>(type: "real", nullable: false, defaultValue: 1f)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StadiumFactors", x => x.Stadium);
+                    table.ForeignKey(
+                        name: "FK_StadiumFactors_Stadiums_Stadium",
+                        column: x => x.Stadium,
+                        principalTable: "Stadiums",
+                        principalColumn: "StadiumId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StadiumFactors");
+        }
+    }
+}
---
+++ VKR.EF.Entities/Tables/StadiumFactor.cs
@@ -0,0 +1,13 @@
+﻿namespace VKR.EF.Entities
+{
+    public class StadiumFactor
+    {
+        public Stadium Stadium { get; set; }
+        public ushort StadiumId { get; set; }
+        public float HittingFactor { get; set; }
+        public float SingleFactor { get; set; }
+        public float DoubleFactor { get; set; }
+        public float TripleFactor { get; set; }
+        public float HomeRunFactor { get; set; }
+    }
+}
