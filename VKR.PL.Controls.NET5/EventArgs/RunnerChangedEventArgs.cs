dd5243b4934086c982384fd76d5941333fc8534b
Danila Kalugin <danilacalugin34@gmail.com>
2023-01-11 15:02:24 +0300

    Updated namespace VKR.PL.Controls.EventArgs

---
+++ VKR.PL.Controls.NET5/EventArgs/RunnerChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using VKR.EF.Entities;
+
+namespace VKR.PL.Controls.NET5.EventArgs
+{
+    public class RunnerChangedEventArgs: System.EventArgs
+    {
+        public Runner Runner { get; set; }
+
+        public RunnerChangedEventArgs(Runner runner)
+        {
+            Runner = runner;
+        }
+    }
+}
---
+++ VKR.PL.Controls.NET5/EventArgs/ValuePositionChangedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VKR.PL.Controls.NET5.EventArgs
+{
+    public class ValuePositionChangedEventArgs : System.EventArgs
+    {
+        public ushort Position { get; set; }
+
+        public ValuePositionChangedEventArgs(ushort position) => Position = position;
+    }
+}
